8e9e51db394853b67eca23d4bf27ca2d453f498a
Alena <kirilchik.al@gmail.com>
2022-10-24 19:58:40 +0200

    Change permission to user/register path

--- src/main/java/pwr/edu/pl/travelly/api/security/SecurityConfiguration.java
+++ src/main/java/pwr/edu/pl/travelly/api/security/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/user/register", "/user/authenticate",
+                .antMatchers("/user/register/**", "/user/authenticate",
                         "/swagger-ui/**",
                         "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
