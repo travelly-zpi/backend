5ba2027a5ff184347ffe545ba5d2952b0e7b58ca
Kiryl Kulbachynski <45761618+bulbik2000@users.noreply.github.com>
2022-10-26 20:37:53 +0200

    Update UserController.java

--- src/main/java/pwr/edu/pl/travelly/api/controllers/UserController.java
+++ src/main/java/pwr/edu/pl/travelly/api/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
